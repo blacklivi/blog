060e7eaa562bb91fc05d9fb5125376bd9a73933d
blacklivi <blacklivi@yandex.by>
2021-03-03 20:49:34 +0300

    init progect

---
+++ nuxt.config.js
@@ -0,0 +1,34 @@
+export default {
+  head: {
+    title: 'nuxt-blog',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  css: [
+    'element-ui/lib/theme-chalk/index.css',
+    '@/theme/index.css'
+  ],
+  plugins: [
+    '@/plugins/globals'
+  ],
+  components: true,
+  buildModules: [
+    '@nuxtjs/eslint-module'
+  ],
+  modules: [
+    '@nuxtjs/axios'
+  ],
+  axios: {},
+  build: {
+    transpile: [/^element-ui/]
+  }
+}
