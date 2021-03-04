2941e0688e5cac58985d05a2cbd27657f99fa865
blacklivi <blacklivi@yandex.by>
2021-03-04 14:39:59 +0300

    comment block

--- components/main/Post.vue
+++ components/main/Post.vue
@@ -1,18 +1,69 @@
 <template>
-<article class="post" >
-</article>
+  <el-card
+    shadow="hover"
+    :body-style="{padding: 0}"
+    class="post"
+  >
+    <header slot="header" class="post-header">
+      <h3>
+        post title
+      </h3>
+      <small>
+        <i class="el-icon-time"></i>
+        {{ new Date().toLocaleString() }}
+      </small>
+    </header>
+    <div class="post-body">
+      <img
+        src="https://zakavto.com.ua/images/data/articles/Albania_222-min.png"
+        alt="post image"
+        class="post-img"
+      >
+    </div>
+    <footer class="post-footer">
+      <el-button round @click="openPost">
+        Открыть
+      </el-button>
+      <span>
+        <i class="el-icon-message">
+          12
+        </i>
+      </span>
+    </footer>
+  </el-card>
 </template>
 
 <script>
 export default {
-
+  methods: {
+    openPost () {
+      const id = 'test-id'
+      this.$router.push(`/post/${id}`)
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
 .post {
-  height: 200px;
-  border: 1px solid black;
-  margin-bottom: 2rem;
+  margin-bottom: 1.5rem;
+}
+
+.post-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.post-img {
+  width: 100%;
+  height: auto;
+}
+
+.post-footer {
+  padding: 1rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 </style>
---
+++ pages/post/_id.vue
@@ -0,0 +1,142 @@
+<template>
+  <article class="post">
+    <header class="post-header">
+      <div class="post-title">
+        <h1>Post title</h1>
+        <nuxt-link to="/">
+          <i class="el-icon-back"></i>
+        </nuxt-link>
+      </div>
+      <div class="post-info">
+        <small>
+          <i class="el-icon-time"></i>
+          {{ new Date().toLocaleString() }}
+        </small>
+        <small>
+          <i class="el-icon-view"></i>
+          42
+        </small>
+      </div>
+      <div class="post-image">
+        <img
+          src="https://zakavto.com.ua/images/data/articles/Albania_222-min.png"
+          alt="post image"
+        >
+      </div>
+    </header>
+    <main class="post-content">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+        molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+        numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+        optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
+        obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
+        nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
+        tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
+        quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos
+        sapiente officiis modi at sunt excepturi expedita sint?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+        molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+        numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+        optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
+        obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
+        nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
+        tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
+        quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos
+        sapiente officiis modi at sunt excepturi expedita sint?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+        molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+        numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+        optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
+        obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
+        nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
+        tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
+        quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos
+        sapiente officiis modi at sunt excepturi expedita sint?
+      </p>
+    </main>
+    <footer>
+      <app-comment-form
+        v-if="canAddComment"
+        @created="createCommentHandler"
+      />
+
+      <div
+        class="comments"
+        v-if="true"
+      >
+        <app-comment
+          v-for="comment in 4"
+          :key="comment"
+          :comment="comment"
+        />
+      </div>
+      <div class="text-centre" v-else>
+        Комметариев нет
+      </div>
+    </footer>
+  </article>
+</template>
+
+<script>
+import AppComment from '@/components/main/Comment'
+import AppCommentForm from '@/components/main/CommentForm'
+
+export default {
+  validate ({ params }) {
+    return Boolean(params.id)
+  },
+  data () {
+    return {
+      canAddComment: true
+    }
+  },
+  methods: {
+    createCommentHandler () {
+      this.canAddComment = false
+    }
+  },
+  components: {
+    AppComment,
+    AppCommentForm
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.post {
+  max-width: 600px;
+  margin: 0 auto;
+}
+
+.post-title {
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 1rem;
+  align-items: center;
+}
+
+.post-info {
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 0.5rem;
+  align-items: center;
+}
+
+.post-image img {
+  width: 100%;
+  height: auto;
+}
+
+.post-header {
+  margin-bottom: 1.5rem;
+}
+
+.post-content {
+  margin-bottom: 2rem;
+}
+</style>
